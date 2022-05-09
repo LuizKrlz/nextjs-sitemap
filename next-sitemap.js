adf9b9173c5d7157127fa0c6bd9535dccea17fce
Luiz Correa <luizcorrea@MacBook-Pro-de-Luiz.local>
2022-05-09 12:19:13 -0300

    second commit

--- next-sitemap.js
+++ next-sitemap.js
@@ -3,4 +3,5 @@
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://nextjs-sitemap-capfj29yj-luizcorreadev.vercel.app/',
     generateRobotsTxt: true, // (optional)
+    outDir: '.next/static/pages'
   }
